fd6bfa719e5f500fd2e9ff1c786dce4bbc72cacd
Klrushka <comeindonotbeshy@gmail.com>
2021-08-26 15:57:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        /*
+        Exercise 1: (2) Implement a Runnable. Inside run( ), print a message, and then call
+        yield( ). Repeat this three times, and then return from run( ). Put a startup message in the
+        constructor and a shutdown message when the task terminates. Create a number of these
+        tasks and drive them using threads.
+         */
+
+
+
+
+    }
+}
