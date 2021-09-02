4fddd524d451187bc150d3dd25f2356f02823232
Klrushka <comeindonotbeshy@gmail.com>
2021-09-02 21:14:54 +0300

    <----- ^-^

--- src/Main.java
+++ src/Main.java
@@ -120,8 +120,12 @@ public class Main {
 
 
         /*
-
+        Exercise 35: (8) Modify BankTellerSimulation.java so that it represents Web clients
+        making requests of a fixed number of servers. The goal is to determine the load that the
+        group of servers can handle.
          */
+
+
 
 
 
